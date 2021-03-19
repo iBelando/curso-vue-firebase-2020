340ecb4d03154ac6ced5cb4913e26333452186ca
iBelando <ignaciobelando@gmail.com>
2021-03-18 23:38:17 -0300

    6. Filtros

---
+++ 6. Filtros/codigo.js
@@ -0,0 +1,42 @@
+const app = new Vue({
+    el: '#main',
+    data() {
+        return {
+            tarea: null,
+            tareas: [
+                {
+                    titulo: 'Aprender ES6',
+                    completado: false
+                },
+                {
+                    titulo: 'Aprender Vue',
+                    completado: true
+                },
+                {
+                    titulo: 'Desarrollar toda la tarde',
+                    completado: false
+                },
+                {
+                    titulo: 'Levantate',
+                    completado: true
+                }
+            ]
+        }
+    },
+    methods: {
+        agregarTarea() {
+            this.tareas.unshift({ titulo: this.tarea, completado: false });
+        }
+    },
+    computed: {
+        mostrarCompletadas() {
+            return this.tareas.filter(item => item.completado);
+        },
+        mostrarNombre() {
+            return "Manuel";
+        },
+        mostrarPorTitulo() {
+            return this.tareas.filter(item => item.titulo.includes(this.tarea));
+        }
+    },
+})
