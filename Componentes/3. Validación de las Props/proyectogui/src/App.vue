e780206cd4ed48db4f08cf5d58e51c1c6c98de64
iBelando <ignaciobelando@gmail.com>
2021-03-23 20:00:24 -0300

    3. Validación de las Props

---
+++ Componentes/3. Validación de las Props/proyectogui/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Usuarios v-for="usuario in usuarios" :nombre="usuario.name" :email="usuario.email" :telefono="usuario.phone" :key="usuario.id" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Usuarios from './components/Usuarios';
+
+export default {
+  name: 'App',
+  components: {
+    Usuarios
+  },
+  data() {
+    return {
+      usuarios: []
+    }
+  },
+  methods: {
+    obtenerUsuarios () {
+      axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        this.usuarios = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  },
+  mounted() {
+    this.obtenerUsuarios();
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Componentes/3. Validación de las Props/proyectogui/src/components/Usuarios.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>Nombre: {{ nombre }}</p>
+        <p>Email: {{ email }}</p>
+        <p>Teléfono: {{ telefono }}</p>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Usuarios',
+    props: {
+        nombre: {
+            type: String,
+            required: true,
+            default: 'John'
+        },
+        email: String,
+        telefono: Number
+    }
+}
+</script>
+
+<style>
+
+</style>
