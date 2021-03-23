625a6ae4af218e81f127942ad1c25bd6ac9bf32b
iBelando <ignaciobelando@gmail.com>
2021-03-23 20:26:45 -0300

    4. Slots y NameSlots

---
+++ Componentes/4. Slots y NameSlots/proyectogui/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <ComponenteSlot>
+      Hola desde el slot
+      
+      <!-- Para modificar un slot, es necesario utilizar la etiqueta "template" -->
+      <template slot="slot2">
+        Hola desde el slot 2
+      </template>
+
+      <template slot="slot3">
+        Hola desde el slot 3
+      </template>
+    </ComponenteSlot>
+  </div>
+</template>
+
+<script>
+import ComponenteSlot from './components/ComponenteSlot';
+
+export default {
+  name: 'App',
+  components: {
+    ComponenteSlot
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Componentes/4. Slots y NameSlots/proyectogui/src/components/ComponenteSlot.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1 style="color: red;">
+            <!-- Entre las etiquetas "slot", va todo lo que es editable en el componente desde afuera -->
+            <slot>
+                Hola desde el componente
+            </slot>
+        </h1>
+        <hr>
+        <h2>
+            <!-- Para multiples slots, es necesario definirlo un nombre a cada slot -->
+            <slot name="slot2">
+                Hola desde el componente 2
+            </slot>
+        </h2>
+        <hr>
+        <h2>
+            <slot name="slot3">
+                Hola desde el componente 3
+            </slot>
+        </h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponenteSlot'
+}
+</script>
+
+<style>
+
+</style>
