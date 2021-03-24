39d91dc9b60197e2fd6697d54ba2143712890035
iBelando <ignaciobelando@gmail.com>
2021-03-23 21:03:15 -0300

    6. La función Render

---
+++ Componentes/6. La función Render/proyectogui/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Elemento tipo="h1">Texto de este elemento</Elemento>
+    <Elemento tipo="h2">Texto de este elemento</Elemento>
+    <Elemento tipo="h3">Texto de este elemento</Elemento>
+    <Elemento tipo="aside">Contenido de Aside</Elemento>
+    <Elemento tipo="h1">Contenido de Aside</Elemento>
+    <Elemento tipo="p">Contenido de Aside</Elemento>
+  </div>
+</template>
+
+<script>
+import Elemento from './components/Elemento';
+
+export default {
+  name: 'App',
+  components: {
+    Elemento
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Componentes/6. La función Render/proyectogui/src/components/Elemento.vue
@@ -0,0 +1,19 @@
+
+<script>
+export default {
+    name: 'Elemento',
+    props: {
+        tipo: String
+    },
+    render(h) {
+        return h(
+            this.tipo, // Que tipo de elemento va a renderizar
+            this.$slots.default //  Contenido del elemeneto a renderizar   
+            )
+    },
+}
+</script>
+
+<style>
+
+</style>
