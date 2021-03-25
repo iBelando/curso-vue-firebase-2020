4755165a7f62961505a78f5e0354aad24f45e222
iBelando <ignaciobelando@gmail.com>
2021-03-25 20:36:07 -0300

    1. Las Rutas en Vue

---
+++ 4. Rutas/1. Las Rutas en Vue/proyectogui/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>6 Pasos para Trabajar con Rutas en Vue</h1>
+    <router-link to="/">Inicio</router-link>
+    <hr>
+    <router-link to="/blog">Blog</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 4. Rutas/1. Las Rutas en Vue/proyectogui/src/components/Inicio.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Inicio</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Inicio'
+}
+</script>
+
+<style>
+
+</style>
