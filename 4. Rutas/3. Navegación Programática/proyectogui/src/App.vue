381f813b39f1b8864286762231d33f36559b5b46
iBelando <ignaciobelando@gmail.com>
2021-03-25 21:36:23 -0300

    3. Navegación Programática

---
+++ 4. Rutas/3. Navegación Programática/proyectogui/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>6 Pasos para Trabajar con Rutas en Vue</h1>
+    <router-link active-class="activo" tag="li" exact to="/">
+      <a>Inicio</a>
+    </router-link>
+    <hr>
+    <router-link active-class="activo" tag="li" to="/blog">
+      <a>Blog</a>
+    </router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  },
+}
+</script>
+
+<style>
+.activo a {
+  color: orange;
+
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 4. Rutas/3. Navegación Programática/proyectogui/src/routes.js
@@ -0,0 +1,7 @@
+import Inicio from './components/Inicio.vue';
+import Blog from './components/Blog.vue';
+
+export const routes = [
+    { path: '/', component: Inicio },
+    { path: '/blog', component: Blog }
+]
