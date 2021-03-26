ee23ec657a669c3fe58011bd0f03940169ddc303
iBelando <ignaciobelando@gmail.com>
2021-03-25 21:52:16 -0300

    4. Rutas Dinámicas

---
+++ 4. Rutas/4. Rutas Dinámicas/proyectogui/src/components/Posts.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Recepción del parámetro de la ruta</h1>
+        <p>{{ id }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Posts',
+    data() {
+        const {id} = this.$route.params;
+        return {
+            id
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
